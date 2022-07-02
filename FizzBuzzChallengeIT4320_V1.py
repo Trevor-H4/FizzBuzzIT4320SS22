cc5b57dcfc073ae4ce1c634b78db62a836d7d854
Trevor-H4 <60674670+Trevor-H4@users.noreply.github.com>
2022-07-01 20:01:17 -0500

    Delete FizzBuzzChallengeIT4320_V1.py

--- FizzBuzzChallengeIT4320_V1.py
+++
@@ -1,9 +0,0 @@
-for testVar in range(1,101):
-    if testVar % 3 == 0 and testVar % 5 == 0:
-        print("fizzbuzz")
-    elif testVar % 3 == 0:
-        print("fizz")
-    elif testVar % 5 == 0:
-        print("buzz")
-    else:
-        print(testVar)
